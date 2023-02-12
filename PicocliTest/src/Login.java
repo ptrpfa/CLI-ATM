ffa8c89d13f5ca07e34dee5568706a83af4b2159
skyish242 <49118372+skyish242@users.noreply.github.com>
2023-02-12 20:16:17 +0800

    Test

---
+++ PicocliTest/src/Login.java
@@ -0,0 +1,27 @@
+import picocli.CommandLine;
+import java.io.Console;
+import java.util.Scanner;
+
+@CommandLine.Command(name = "login")
+class Login implements Runnable {
+    @Override
+    public void run() {
+        String[] banner = new CommandLine(new Main()).getCommandSpec().usageMessage().header();
+        for (String line : banner) {
+            //AnsiConsole.systemInstall();
+            System.out.println(CommandLine.Help.Ansi.ON.string(line));
+            //AnsiConsole.systemUninstall();
+        }
+        main();
+    }
+    public static void main(){
+        Scanner scanner = new Scanner(System.in);
+        Console console = System.console();
+        System.out.println("欢迎来到本次银行!");
+//        System.out.println("\n请输入密码：");
+        String password = new String(console.readPassword("\n请输入密码："));
+        System.out.println(password);
+        System.out.println("\n请稍等...正搜查您的账本~");
+
+    }
+}
---
+++ PicocliTest/src/header.java
@@ -0,0 +1,26 @@
+import picocli.CommandLine.Help.Ansi;
+//import org.fusesource.jansi.AnsiConsole; //Either this or the picocli one
+import picocli.jansi.graalvm.AnsiConsole;
+
+public class header {
+    //can change this to any ASCII ART
+    //ASCII Art Generator: https://patorjk.com/software/taag/#p=display&f=Graffiti&t=
+    public static final String[] header = {
+            "@|green .____    .__         ____  __.      .__  __________.__|@",
+            "@|green |    |   |__|__ __  |    |/ _|____  |__| \\______   \\__| ____    ____  |@",
+            "@|green |    |   |  |  |  \\ |      < \\__  \\ |  |  |     ___/  |/    \\  / ___\\ |@",
+            "@|green |    |___|  |  |  / |    |  \\ / __ \\|  |  |    |   |  |   |  \\/ /_/  >|@",
+            "@|green |_______ \\__|____/  |____|__ (____  /__|  |____|   |__|___|  /\\___  / |@",
+            "@|green         \\/                  \\/    \\/                       \\//_____/  |@",
+    };
+    public static void Print(){
+        //Install ANSI command for Windows
+        AnsiConsole.systemInstall();
+        for(String x:header){
+            System.out.println(Ansi.ON.string(x));
+        }
+        //Clean Up ANSI command
+        AnsiConsole.systemUninstall();
+    }
+
+}
