873a56b4a0c98c27890b06567741e60281722e95
Peter <30462819+ptrpfa@users.noreply.github.com>
2023-02-15 17:55:41 +0800

    outline

---
+++ ATM/Database.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+// Main class for Database connections
+public class Database {
+
+    public static void main(String[] args) {
+        System.out.println("Test!");
+    }
+
+}
