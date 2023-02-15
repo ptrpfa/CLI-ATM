4abf0e9c47896d2379b6d7a85ae7439710d9c11a
Peter <30462819+ptrpfa@users.noreply.github.com>
2023-02-16 01:15:26 +0800

    added tests of jdbc connection for CRU

--- ATM/Database.java
+++ ATM/Database.java
@@ -3,7 +3,7 @@ package ATM;
 
 // Imports
 import java.io.*;
-import java.util.Properties;
+import java.util.*;
 import java.sql.*;
 
 // Main class for Database connections
@@ -12,9 +12,10 @@ public class Database {
    // public static final String CONFIGURATION_FILE = "settings.config";
    public static final String CONFIGURATION_FILE = "settings_prod.config";
 
-
+   // Program entrypoint
    public static void main(String[] args) {
 
+      /* Load configuration settings */
       // Create a FileInputSteam object to read configuration settings file
       FileInputStream settingsFile = null;
       
@@ -48,51 +49,134 @@ public class Database {
       final String CONNECTION_USER = propSettings.getProperty("DB_USER");
       final String CONNECTION_PASSWORD = propSettings.getProperty("DB_PASSWORD");
 
+
+      /* Create database connection */
       try {
 
-            // Step 1: Construct a database 'Connection' object called 'conn'
-            Connection conn = DriverManager.getConnection(CONNECTION_URL, CONNECTION_USER, CONNECTION_PASSWORD);   // For MySQL only
+         // Create Connection object to database
+         Connection dbConnection = DriverManager.getConnection(CONNECTION_URL, CONNECTION_USER, CONNECTION_PASSWORD); 
 
-            // Step 2: Construct a 'Statement' object called 'stmt' inside the Connection created (SHOULD USE PREPARED STATEMENT)
-            Statement stmt = conn.createStatement();
+         // SQL Statements for testing
+         String query = "SELECT * FROM User;";
+         String insert = "INSERT INTO User VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+         String update = "UPDATE User SET Phone=\"+65 12345678\" WHERE Username LIKE ?"; // Add %?% during setting
 
-            // Step 3: Write a SQL query string. Execute the SQL query via the 'Statement'.
-            //  The query result is returned in a 'ResultSet' object called 'rset'.
-            String strSelect = "SELECT * FROM User;";
-            System.out.println("The SQL statement is: " + strSelect + "\n"); // Echo For debugging
+         // Declare a PreparedStatement object
+         PreparedStatement prepStatement;
 
-            ResultSet rset = stmt.executeQuery(strSelect);
+         /* QUERY */
+         prepStatement = dbConnection.prepareStatement(query);
+         ResultSet results = prepStatement.executeQuery();
+         // Loop through results of query
+         while(results.next()) {   
+            // Get column values
+            int userid = results.getInt("UserID");
+            String username = results.getString("Username");
+            String salt = results.getString("PasswordSalt");
+            String hash = results.getString("PasswordHash");
+            String email = results.getString("Email");
+            String phone = results.getString("Phone");
+            String addressOne = results.getString("AddressOne");
+            String addressTwo = results.getString("AddressTwo");
+            String addressThree = results.getString("AddressThree");
+            String postal = results.getString("PostalCode");
+            Timestamp timestamp = results.getTimestamp("RegistrationDate");
+            java.util.Date datetime = new java.util.Date(timestamp.getTime());
+            boolean active = results.getInt("Active") > 0 ? true : false;
 
-            // Step 4: Process the 'ResultSet' by scrolling the cursor forward via next().
-            //  For each row, retrieve the contents of the cells with getXxx(columnName).
-            System.out.println("The records selected are:");
-            int rowCount = 0;
-            // Row-cursor initially positioned before the first row of the 'ResultSet'.
-            // rset.next() inside the whole-loop repeatedly moves the cursor to the next row.
-            // It returns false if no more rows.
-            while(rset.next()) {   // Repeatedly process each row
-               String title = rset.getString("username");  // retrieve a 'String'-cell in the row
-               String price = rset.getString("email");  // retrieve a 'double'-cell in the row
-               String    qty   = rset.getString("passwordhash");       // retrieve a 'int'-cell in the row
-               System.out.println(title + ", " + price + ", " + qty);
-               ++rowCount;
+            // Print values
+            System.out.printf("\n%s %s %s %s %s %s %s %s %s %s %s %s\n", userid, username, salt, hash, email, phone, addressOne ,addressTwo, addressThree, postal, datetime, active);
          }
 
-         System.out.println("Total number of records = " + rowCount);
+         /* INSERT */
+         // Hash Map of column values
+         LinkedHashMap<String, Object> linkedhashmap = new LinkedHashMap<String, Object>();
+         linkedhashmap.put("UserID", 11);
+         linkedhashmap.put("Username", "HELLO HELLO");
+         linkedhashmap.put("PasswordSalt", "pepper");
+         linkedhashmap.put("PasswordHash", "hash");
+         linkedhashmap.put("Email", "email@ello.com");
+         linkedhashmap.put("Phone", "8593883");
+         linkedhashmap.put("AddressOne", "a1");
+         linkedhashmap.put("Address2", "a1");
+         linkedhashmap.put("Address3", "a1");
+         linkedhashmap.put("PostalCode", "testaddition");
+         linkedhashmap.put("RegistrationDate", new java.util.Date());
+         linkedhashmap.put("Active", 0);
+
+         prepStatement = dbConnection.prepareStatement(insert);
+
+         // Automatic setting of variable datatype argument list
+         int index = 1;
+         // Loop through each item in the hashmap
+         for(Map.Entry<String, Object> entry : linkedhashmap.entrySet()) {
+
+            String key = entry.getKey();
+            Object value = entry.getValue();
+
+            // check datatypes
+            if(entry.getValue() instanceof String) {
+               prepStatement.setString(index, value.toString());
+               index++;
+            }
+            if(entry.getValue() instanceof Integer) {
+               prepStatement.setInt(index, (int)value);
+               index++;
+            }
+            else if(entry.getValue() instanceof Double) {
+               prepStatement.setDouble(index, (Double)value);
+               index++;
+            }
+            else if(entry.getValue() instanceof java.util.Date) {
+               java.util.Date dateValue = (java.util.Date) value;
+               Timestamp timestampValue = new Timestamp(dateValue.getTime());
+               prepStatement.setTimestamp(index, timestampValue);
+               index++;
+            }
+            else if(entry.getValue() instanceof Boolean) {
+               int intValue = (Boolean)value ? 1 : 0;
+               prepStatement.setInt(index, intValue);
+               index++;
+            }
+
+         }
+
+         System.out.println(prepStatement.executeUpdate() + " record effected!");
+
+         /* UPDATE */
+         prepStatement = dbConnection.prepareStatement(update);
+         prepStatement.setString(1, "%" + "ggy" + "%");
+         System.out.println(prepStatement.executeUpdate() + " record effected!");
+
+         // Close connection
+         if(dbConnection != null) {
+            dbConnection.close();
+         }
 
       } 
-      
-      catch(SQLException ex) {
-
-         ex.printStackTrace();
-
-      }  // Step 5: Close conn and stmt - Done automatically by try-with-resources (JDK 7)
-      catch(Exception e) {
-
-         System.out.println(e);
-      
+      catch(SQLException exception) {
+         // Terminate program 
+         exception.printStackTrace();
+         System.exit(-1);
+      } 
+      catch(Exception exception) { // Catch miscellaneous exceptions
+         // Terminate program
+         exception.printStackTrace();
+         System.exit(-1);
       }
 
    }
+
+   // Interface implementations
+
+   // Read (SELECT)
+   // 
+
+   // Create
+   // public abstract boolean createDB()
+
+   // Update
+
+   // Delete
 
 }
