fe07efd619091e14519d63c71affecb63178b954
JR <Jianrong12@outlook.com>
2023-03-02 12:51:52 +0800

    j

--- database/archive/test2.sql
+++ database/archive/test2.sql
@@ -2,3 +2,4 @@ UPDATE `OOP_ATM`.`User` SET Username = 'Liu Kai Ping', PasswordHash = 'pI3IEoY0Z
 SELECT * FROM `OOP_ATM`.`User` LIMIT 1000;
 SELECT * FROM `OOP_ATM`.`NormalUser` LIMIT 1000;
 SELECT * FROM `OOP_ATM`.`BusinessUser` LIMIT 1000;
+SELECT * FROM `OOP_ATM`.`Account` LIMIT 100; WHERE UserID = 833020;
